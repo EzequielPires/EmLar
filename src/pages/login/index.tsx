fc4e10d36bebaca372e7c4c52a3a065d7ac740a1
EzequielPires <ezequiel.pires082000@gmail.com>
2022-04-04 23:17:14 -0300

    Commit

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -12,7 +12,6 @@ export default function Login() {
     const handleSign = (e) => {
         e.preventDefault();
         router.push('/admin');
-        
     }
     return (
         <div className={styles.container}>
